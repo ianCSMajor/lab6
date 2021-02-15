78d2459141d182819aa7351af2151a8d37de0cd4
ianCSMajor <brucewinter123@gmail.com>
2021-02-15 13:46:16 -0800

    Added area class

--- src/rectangle.cpp
+++ src/rectangle.cpp
@@ -1,11 +1,15 @@
 #include "rectangle.hpp"
 
+
 Rectangle::Rectangle()
   :width(0), height(0){}
   
 Rectangle::Rectangle(int w, int h){
   :width(w), height(h){}
-  
+
+int Rectangle::area(){
+	return (width * height);
+}  
 int Rectangle::perimeter(){
-return (width + height);
+	return (width + height);
 }
