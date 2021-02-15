a7e641ce462e545a2e39eebcc45f20004d38b406
ianCSMajor <57507406+ianCSMajor@users.noreply.github.com>
2021-02-15 12:10:10 -0800

    Update rectangle.cpp

---
+++ src/rectangle.cpp
@@ -0,0 +1,10 @@
+#include "rectangle.hpp"
+
+Rectangle::Rectangle()
+  :width(0), height(0){};
+  
+Rectangle::Rectangle(int w, int h){
+  :width(w), height(h){};
+  
+int Rectangle::perimeter(){
+return (width + height);
