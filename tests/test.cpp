9ee8abe61550818a7911e168d09d34e5862c6bd7
ianCSMajor <brucewinter123@gmail.com>
2021-02-17 09:57:36 -0800

    Completed User Tests

--- tests/test.cpp
+++ tests/test.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-TEST(Constructor, data_type){
+TEST(ConstructorTest, data_type){
 Rectangle * data = new Rectangle(10, 13);
 EXPECT_EQ(13, data->getHeight());
 EXPECT_EQ(10, data->getWidth());
@@ -12,7 +12,7 @@ delete data;
 data = nullptr;
 }
 
-TEST(Constructor, ConstructTestPerimeter) {
+TEST(ConstructorTest, ConstructTestPerimeter) {
 	Rectangle *per = new Rectangle(5, 3);
 	EXPECT_NEAR(per->perimeter(), 16, 0.01);
 	delete per;
@@ -37,6 +37,62 @@ EXPECT_NEAR(rect.perimeter(), 30, 0.01); }
 TEST(ConstructTest, ConstructTestArea3) {     
 Rectangle rect(6, 9);     
 EXPECT_NEAR(rect.area(), 54, 0.01); } 
+
+TEST(PerimeterTest, perim) {
+Rectangle pos;
+pos.setWidth(3);
+pos.setHeight(4);
+EXPECT_NEAR(pos.perimeter(), 14, 0.01);
+}
+TEST(PerimeterTest, Negative){
+Rectangle pos1;
+pos1.setWidth(-1);
+pos1.setHeight(5);
+
+if(pos1.getWidth() <= 0){
+pos1.setWidth(1);
+}
+EXPECT_NEAR(pos1.perimeter(), 12, 0.01);
+}
+
+TEST(PerimeterTest, zero){
+
+Rectangle zero;
+
+zero.setWidth(0);
+zero.setHeight(4);
+if(zero.getWidth() <= 0){
+zero.setWidth(1);
+}
+EXPECT_NEAR(zero.perimeter(), 10, 0.01);
+}
+
+TEST(AreaTest, Positive){
+Rectangle areaObj;
+areaObj.setWidth(9);
+areaObj.setHeight(4);
+
+EXPECT_NEAR(areaObj.area(), 36, 0.01);
+}
+TEST(AreaTest, Negative){
+Rectangle negObj;
+negObj.setWidth(-99);
+negObj.setHeight(5);
+if(negObj.getWidth() <=0){
+negObj.setWidth(1);
+}
+EXPECT_NEAR(negObj.area(), 5, 0.01);
+}
+
+TEST(AreaTest, Zero){
+Rectangle zeroObj;
+zeroObj.setWidth(0);
+zeroObj.setHeight(3);
+if(zeroObj.getWidth() <= 0){
+zeroObj.setWidth(1);
+}
+EXPECT_NEAR(zeroObj.area(), 3, 0.01);
+}
 
 int main(int argc, char **argv) {   
 ::testing::InitGoogleTest(&argc, argv);   
